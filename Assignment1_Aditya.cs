bc2187ec82177ff838cf6d6dddfda65842fc8166
Aditya <126596327+Adityakulkarni11@users.noreply.github.com>
2025-07-25 16:47:39 +0530

    1st Assignment

---
+++ Assignment1_Aditya.cs
@@ -0,0 +1,21 @@
+﻿namespace Assignments
+{
+    internal class Assignment1_Aditya
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Integral Data Types and their range:");
+            Console.WriteLine($"int : {int.MinValue} to {int.MaxValue}");
+            Console.WriteLine($"byte : {byte.MinValue} to {byte.MaxValue}");
+            Console.WriteLine($"short : {short.MinValue} to {short.MaxValue}");
+            Console.WriteLine($"long : {long.MinValue} to {long.MaxValue}");
+            Console.WriteLine($"char : {(int)char.MinValue} to {(int)char.MaxValue}");
+            Console.WriteLine();
+            Console.WriteLine("Floating Data Types and their range:");
+            Console.WriteLine($"float : {float.MinValue} to {float.MaxValue}");
+            Console.WriteLine($"double : {double.MinValue} to {double.MaxValue}");
+            Console.WriteLine($"decimal : {decimal.MinValue} to {decimal.MaxValue}");
+            
+        }
+    }
+}
---
+++ Assignment2_Aditya.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Assignments
+{
+    internal class Assignment2_Aditya
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of the array : ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Enter the array elements : ");
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            EvenOdd(arr);
+        }
+
+        private static void EvenOdd(int[] arr)
+        {
+            Console.Write("Even Numbers : ");
+            for (int i = 0; i < arr.Length; i++) { 
+                if(arr[i]%2==0 )
+                {
+                    Console.Write(arr[i]+" ");
+                }
+            }
+            Console.WriteLine();
+            Console.Write("Odd Numbers : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 != 0)
+                {
+                    Console.Write(arr[i]+" ");
+                }
+            }
+        }
+    }
+}
